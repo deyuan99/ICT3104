cdb628b7d6b371b48e0e2897eab0d17af01e4f57
Arifah <nuruliffyne@gmail.com>
2017-10-03 14:47:30 +0800

    Add trainer list page for Trainee

---
+++ trainee_trainerList.php
@@ -0,0 +1,81 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>STPS</title>
+        <link rel="stylesheet" href="assets/css/main.css" />
+        <link rel="stylesheet" href="fullcalendar-3.5.1/fullcalendar.css" />
+    </head>
+
+    <body>
+        <!-- Header -->
+        <?php
+        include "trainee_header.php";
+        ?>
+        <!-- Trainers list -->
+        <section id="main" class="wrapper">
+            <div class="container">
+                <header class="major special">
+                    <h3>Trainer List</h3>
+                    <p>Look for a trainer A</p>
+                </header>
+
+                <section>
+                    <div class="table-wrapper">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Trainer's Name</th>
+                                    <th>Category</th>
+                                    <th>Profile</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Trainer 1</td>
+                                    <td>some decription</td>
+                                    <td><a href="#" class="button small">View</a></td>
+                                </tr>
+                                <tr>
+                                    <td>Trainer 2</td>
+                                    <td>some decription</td>
+                                    <td><a href="#" class="button small">View</a></td>
+                                </tr>
+                                <tr>
+                                    <td>Trainer 3</td>
+                                    <td>some decription</td>
+                                    <td><a href="#" class="button small">View</a></td>
+                                </tr>
+                                <tr>
+                                    <td>Trainer 4</td>
+                                    <td>some decription</td>
+                                    <td><a href="#" class="button small">View</a></td>
+                                </tr>
+                                <tr>
+                                    <td>Trainer 5</td>
+                                    <td>some decription</td>
+                                    <td><a href="#" class="button small">View</a></td>
+                                </tr>
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                   <!- pagination -->
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                </section>
+            </div>
+        </section>
+
+        <!-- Footer -->
+        <?php
+        include "footer.php";
+        ?>
+
+        <!-- Scripts -->
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/skel.min.js"></script>
+        <script src="assets/js/util.js"></script>
+        <script src="assets/js/main.js"></script>
+    </body>
+</html>
