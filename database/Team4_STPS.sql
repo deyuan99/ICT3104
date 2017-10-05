ff657aa5e82fc40489830dc85ab43125958543f5
FangDi Fu <yellow6629@hotmail.com>
2017-10-05 22:40:22 +0800

    Update of database name
    change of database name in .sql because dbconfig database name is the
    caps.

--- database/Team4_STPS.sql
+++ database/Team4_STPS.sql
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `team4_stps`
+-- Database: `Team4_STPS`
 --
 
 -- --------------------------------------------------------
