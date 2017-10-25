7860ddb52918d8970ce210dc4ce9ad25bde8b653
Arifah <nuruliffyne@gmail.com>
2017-10-25 16:05:47 +0800

    view trainerList connected to database.

---
+++ listTrainers.php
@@ -0,0 +1,28 @@
+<?php
+
+session_start();
+include_once 'database/dbconfig.php';
+$id = $_SESSION['email'];
+
+$sqlTrainerName = "SELECT * FROM users WHERE role='trainer';";
+$result = $conn->prepare($sqlTrainerName);
+$result->execute();
+
+$count = $result->rowCount();
+
+if ($count > 0) {
+    while ($trainer = $result->fetch(PDO::FETCH_ASSOC)) {
+        $firstName = $trainer['firstName'];
+        $lastName = $trainer['lastName'];
+        
+        echo "<tbody>";
+        echo "<tr>";
+        echo "<td>" . $firstName ." " . $lastName . "</td>";
+        echo "<td>some decription</td>";
+        echo "<td><a href = 'trainer_dashboard.php' class = 'button small'>View</a></td>";
+        echo "</tr>";
+        echo "</tbody>";
+    }
+} else {
+    echo "There are no Trainers yet!";
+}
