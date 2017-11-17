280465ba93797574de4b80d8378668d47137035c
hongzhow <hong_9898@hotmail.com>
2017-11-17 13:44:54 +0800

    capacity checking for : trainer dash trainee dash do approve / admin approve for group

--- addCalendarEvent.php
+++ addCalendarEvent.php
@@ -4,19 +4,55 @@ session_start();
 require_once('database/dbconfig.php');
 //echo $_POST['title'];
 if (isset($_POST['category']) && isset($_POST['starttime']) && isset($_POST['endtime']) && isset($_POST['description'])){
-	
-        $Semail = $_SESSION['email'];
-        $Sname = $_SESSION['name'];
+            $date = $_POST['date'];
+            $roomtype = $_POST['roomtype'];
+            $venue = $_POST['venue'];
+            $dateformat = date('Y-m-d',strtotime($date));
+            $starttime = $_POST['starttime'];
+            $Semail = $_SESSION['email'];
+            $Sname = $_SESSION['name'];
+
+            $category= $_POST['category'];
+            $endtime = $_POST['endtime'];
+            $description = $_POST['description'];
+
+
+        // (1)get the number from personal training on this date, roomtype and venue and startime
+        $sql6 = "SELECT COUNT(*) as total_rows FROM personalsession p,roomtype r, venue v where p.roomTypeID=r.id and v.id = r.venueID and p.date ='$dateformat' and p.startTime = '$starttime' and r.name = '$roomtype' and v.location = '$venue'";
+        $req1 = $conn->prepare($sql6);
+        $req1->execute();
+        $row = $req1->fetch(PDO::FETCH_ASSOC);
+        $total_rows = $row['total_rows'];
         
-        $date = $_POST['date'];
-        $dateformat = date('Y-m-d',strtotime($date));
-        $category= $_POST['category'];
-        $starttime = $_POST['starttime'];
-        $endtime = $_POST['endtime'];
-        $description = $_POST['description'];
-        $venue = $_POST['venue'];
-        $roomtype = $_POST['roomtype'];
-        
+        //(2) get the group cap base on this date, roomtype and venue and startime
+        $totalgroupcap = 0;
+        $sql9 = "SELECT * FROM groupsession g,roomtype r, venue v where g.roomTypeID=r.id and v.id = r.venueID and date= '$dateformat'and g.startTime = '$starttime' and status = 'Approved' and r.name = '$roomtype' and v.location = '$venue' ";
+        $req2 = $conn->prepare($sql9);
+        $req2->execute();
+         $coungroup = $req2->rowCount();
+        if ($coungroup > 0) {
+                            while ($row2 = $req2->fetch(PDO::FETCH_ASSOC)) {
+                                $grpcap = $row2['groupCapacity'];
+                                $totalgroupcap  = $grpcap + $totalgroupcap;
+         }
+       }
+       
+        //(3)get the number limit base on  roomtype and venue
+         $sql12 = "SELECT capacity FROM roomtype r, venue v where v.id = r.venueID and r.name = '$roomtype' and v.location = '$venue' ";
+         $req3 = $conn->prepare($sql12);
+         $req3->execute();
+         $row3 = $req3->fetch(PDO::FETCH_ASSOC);
+        $capacity = $row3['capacity'];         
+         
+         $currenttotalevent = $totalgroupcap + $total_rows;
+         echo $currenttotalevent;
+        //comparing
+        if ($currenttotalevent>=$capacity){
+             echo "<script type='text/javascript'>alert('Unable to book due to capacity reached');"
+            . "window.location.href='trainee_dashboard.php';"
+            . "</script>";
+        }else{
+   
 	$sql = "INSERT INTO personalsession(category, roomTypeID, startTime, endTime, date, description, traineeEmail) values ('$category', (SELECT id FROM roomtype WHERE name = '$roomtype' AND venueID = (SELECT id FROM venue WHERE location = '$venue')), '$starttime', '$endtime','$dateformat','$description','$Semail')";
         	
 	$query = $conn->prepare( $sql );
@@ -31,16 +67,16 @@ if (isset($_POST['category']) && isset($_POST['starttime']) && isset($_POST['end
 	}
         
         if( $query ){ 
-            echo "<script type='text/javascript'>alert('submitted successfully!');"
-            . "window.location.href='trainee_dashboard.php';"
-            . "</script>";
+         //  echo "<script type='text/javascript'>alert('submitted successfully!');"
+       //     . "window.location.href='trainee_dashboard.php';"
+        //    . "</script>";
           }
         else{
             echo "<script type='text/javascript'>alert('failed');"
                 . "window.location.href='trainee_dashboard.php';"
                  . "</script>";    
             }
-
+        }
 }
 
 	
