5d34a1a19764ad5bf9bfceff241b15105352e008
FangDi Fu <yellow6629@hotmail.com>
2017-11-22 23:23:32 +0800

    update
    remove learn more button and footer icons

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
 <!-- Footer -->
 <footer id="footer">
     <div class="container style2" >
-        <ul class="icons">
+        <!--<ul class="icons">
             <li><a href="#" class="icon fa-facebook"></a></li>
             <li><a href="#" class="icon fa-twitter"></a></li>
             <li><a href="administrator/user-management.php" class="icon fa-instagram"></a></li>
-        </ul>
+        </ul>-->
         <ul class="copyright">
             <li>&copy; ICT3104 Group 4 2017</li>
             <li>Design: <a href="http://templated.co">TEMPLATED</a></li>
--- index.php
+++ index.php
@@ -75,9 +75,7 @@ if (isset($_SESSION['email']) && !empty($_SESSION['email'])) {
                                         <h2>Vestibulum sit amet</h2>
                                     </header>
                                     <h6> Cras aliquet urna ut sapien tincidunt, quis malesuada elit facilisis. Vestibulum sit amet tortor velit. Nam elementum nibh a libero pharetra elementum. Maecenas feugiat ex purus, quis volutpat lacus placerat malesuada.</h6>
-                                    <footer class="align-center">
-                                        <a href="#" class="button alt">Learn More</a>
-                                    </footer>
+                                    
                                 </div>
                             </h3>
                         </div
@@ -93,9 +91,7 @@ if (isset($_SESSION['email']) && !empty($_SESSION['email'])) {
                                         <h2>Vestibulum sit amet</h2>
                                     </header>
                                     <h6> Cras aliquet urna ut sapien tincidunt, quis malesuada elit facilisis. Vestibulum sit amet tortor velit. Nam elementum nibh a libero pharetra elementum. Maecenas feugiat ex purus, quis volutpat lacus placerat malesuada.</h6>
-                                    <footer class="align-center">
-                                        <a href="#" class="button alt">Learn More</a>
-                                    </footer>
+                                    
                                 </div>
                             </h3>
                         </div>
